f00884cc9c09303bcb2e2cdcdebd6fd85733d5d9
Abhay Pratap Yadav <abhaypratapyadav340@gmail.com>
2022-04-23 17:09:55 +0530

    Assests

---
+++ Carburetor 3D/Assets/Scripts/WheelController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider backRight;
+    [SerializeField] WheelCollider backLeft;
+
+    public float acceleration = 500f;
+    public float breakingForce = 300f;
+
+    private float currentAcceleration = 0f;
+    private float currentBreakForce = 0f;
+
+    /*
+    private void FixedUpdate()
+    {
+        // Apply acceleration to front wheels
+        frontRight.motorTorque = currentAcceleration;
+        frontLeft.motorTorque = currentAcceleration;
+
+        // Apply brakes
+        frontRight.brakeTorque = currentBreakForce;
+        frontLeft.brakeTorque = currentBreakForce;
+        backRight.brakeTorque = currentBreakForce;
+        backLeft.brakeTorque = currentBreakForce;
+    }
+    */
+
+    public void accelerateButton()
+    {
+        frontRight.motorTorque = currentAcceleration * Time.fixedDeltaTime;
+        frontLeft.motorTorque = currentAcceleration * Time.fixedDeltaTime;
+    }
+
+    public void brakeButton()
+    {
+        currentBreakForce = breakingForce;
+    }
+
+
+}
