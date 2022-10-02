377bdc6655c0068057f9557d1f514c4ea273cf4d
Abhay Pratap Yadav <98642540+Plycedes@users.noreply.github.com>
2022-10-02 23:59:08 +0530

    Camera Follow 1

---
+++ Carburetor 3D/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 camOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + camOffset;
+    }
+}
